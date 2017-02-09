4c9e414f4bd52183536b994c6431128376668e69
achambaz <achambaz@u-paris10.fr>
2017-02-09 14:17:51 -0800

    Tested (slow) simulation function...

--- R/simulator.R
+++ R/simulator.R
@@ -15,10 +15,7 @@ Simulator <-
         generateMechanism = function(param, family=c("bernoulli", "gaussian")) {
           ## Retrieving arguments
           param <- Arguments$getNumerics(param)
-          if ((length(param)-1)%%3 != 0) {
-            throw("Length of 'param' minus 1 must be a multiple of 3")
-          }
-          memory <- (length(param)-1)/3
+          memory <- length(param)-1
           family <- match.arg(family)
           link <- switch(family,
                          bernoulli=expit,
@@ -27,13 +24,15 @@ Simulator <-
           if (length(param)==1) {
             mechanism <- function(xx=numeric(0), par=param, lnk=link, verbose=FALSE) {
               ## Retrieving arguments
+              verbose <- Arguments$getVerbose(verbose)
               if (!length(xx)==0) {
                 verbose && enter(verbose, "Argument 'xx' not used when argument 'par' has length 1")
                 verbose && exit(verbose)
               }
               par <- Arguments$getNumerics(par)
-              lnk <- Arguments$getFunction(lnk)
-              verbose <- Arguments$getVerbose(verbose)
+              if (mode(lnk)!="function") {
+                throw("Argument 'lnk' must be a link function, not ", mode(lnk))
+              }
               ##
               link(par)
             }
@@ -42,13 +41,19 @@ Simulator <-
               ## Retrieving arguments
               xx <- Arguments$getNumerics(xx)
               par <- Arguments$getNumerics(par)
-              lnk <- Arguments$getFunction(lnk)
+              if (mode(lnk)!="function") {
+                throw("Argument 'lnk' must be a link function, not ", mode(lnk))
+              }
               verbose <- Arguments$getVerbose(verbose)
               if (length(xx)!=length(param)-1) {
                 throw("Length of 'xx' should equal length of 'par' minus one")
               }
               ##
-              link(param[1] + param[-1]%*%t(xx))
+              if (FALSE) {
+                link(param[1] + param[-1]%*%xx)
+              } else {
+                link(param[1] + sum(param[-1]*xx))
+              }
             }
           }
           attr(mechanism, "memory") <- memory
@@ -57,20 +62,21 @@ Simulator <-
         },
         
         simulateBinaryWAY = function(by=1,
-                                     qw=generateMechanism(0, family="gaussian"),
-                                     ga=generateMechanism(0, family="bernoulli"),
-                                     Qy=generateMechanism(0, family="bernoulli"),
+                                     qw=self$generateMechanism(0, family="gaussian"),
+                                     ga=self$generateMechanism(0, family="bernoulli"),
+                                     Qy=self$generateMechanism(0, family="bernoulli"),
                                      intervention=NULL,
                                      verbose=FALSE) {
           ## Retrieving arguments
           by <- Arguments$getInteger(by, c(1, Inf))
-          qw <- validateMechanism(qw)
-          ga <- validateMechanism(ga)
-          Qy <- validateMechanism(Qy)
+          qw <- self$validateMechanism(qw)
+          ga <- self$validateMechanism(ga)
+          Qy <- self$validateMechanism(Qy)
           if (!is.null(intervention)) {
-            intervention <- validateIntervention(intervention)
+            intervention <- self$validateIntervention(intervention)
           }
-
+          verbose <- Arguments$getVerbose(verbose)
+          
           families <- c(W=attr(qw, "family"),
                         A=attr(ga, "family"),
                         Y=attr(Qy, "family"))
@@ -82,7 +88,7 @@ Simulator <-
           rsource <- list(bernoulli=runif,
                           gaussian=rnorm)
 
-          rgen <- list(bernoulli=function(xx, yy){(xx <= yy)},
+          rgen <- list(bernoulli=function(xx, yy){as.integer(xx <= yy)},
                        gaussian=function(xx, yy){xx+yy})
           
           ## verbose
@@ -105,21 +111,21 @@ Simulator <-
           ## first version ## must be very slow; write faster version with "inline" and "Rcpp"?
           ## -------------
           for (ii in 1:by) {
-            past.idx <- retrieveRelevantMemoryWAY("W", ii, memories("W"))
+            past.idx <- self$retrieveRelevantMemoryWAY("W", ii, memories["W"])
             which.pos <- which(past.idx>0)
-            past <- rep(0, memories("W"))
+            past <- rep(0, memories["W"])
             past[which.pos] <- WAY[past.idx[which.pos]]
             WAY[(ii-1)*3+1] <- rgen[[families[1]]](UU[ii, 1], qw(past))
             ##
-            past.idx <- retrieveRelevantMemoryWAY("A", ii, memories("A"))
+            past.idx <- self$retrieveRelevantMemoryWAY("A", ii, memories["A"])
             which.pos <- which(past.idx>0)
-            past <- rep(0, memories("A"))
+            past <- rep(0, memories["A"])
             past[which.pos] <- WAY[past.idx[which.pos]]
             WAY[(ii-1)*3+2] <- rgen[[families[2]]](UU[ii, 2], ga(past))
             ##
-            past.idx <- retrieveRelevantMemoryWAY("Y", ii, memories("Y"))
+            past.idx <- self$retrieveRelevantMemoryWAY("Y", ii, memories["Y"])
             which.pos <- which(past.idx>0)
-            past <- rep(0, memories("Y"))
+            past <- rep(0, memories["Y"])
             past[which.pos] <- WAY[past.idx[which.pos]]
             WAY[(ii-1)*3+3] <- rgen[[families[3]]](UU[ii, 3], Qy(past))
           }
@@ -151,12 +157,16 @@ Simulator <-
           at <- Arguments$getInteger(at, c(1, Inf))
           mem <- Arguments$getInteger(mem, c(0, Inf))
           ##
-          first <- switch(of,
-                          "W"=3*(at-1),
-                          "A"=3*(at-1)+1,
-                          "Y"=3*(at-1)+2)
-          last <- first - mem
-          idx <- seq.int(first, last)
+          if (mem==0) {
+            idx <- integer(0)
+          } else {
+            from <- switch(of,
+                           "W"=3*(at-1),
+                           "A"=3*(at-1)+1,
+                           "Y"=3*(at-1)+2)
+            to <- from - mem + 1
+            idx <- seq.int(from, to)
+          }
           return(idx)
         }
       ),
---
+++ tests/test.simulator.R
@@ -0,0 +1,33 @@
+## testing function "simulator.R"
+library(R6)
+library(R.utils)
+source("../R/simulator.R")
+
+log <- Arguments$getVerbose(-8, timestamp=TRUE)
+
+sim <- Simulator$new()
+
+if (FALSE) {
+  ## 'nobs' iid observations
+  nobs <- 50
+  ## W(t)|Past(W(t))~N(0,1)
+  ## A(t)|Past(A(t))~Ber(0.5)
+  ## Y(t)|Past(Y(t))~Ber(0.5)
+  data.iid <- sim$simulateBinaryWAY(nobs, verbose=log) ## very slow!
+}
+
+if (TRUE) {
+  ## 'nobs' observations
+  nobs <- 250
+  ## W(t)|Past(W(t))~N(0.5*Y(t-1)-0.25*A(t-1)+0.1*W(t-1), 1)
+  ## A(t)|Past(A(t))~Ber(expit(-0.1+0.1*W(t)+0.25*Y(t-1)))
+  ## Y(t)|Past(Y(t))~Ber(expit(0.1+0.1*A(t)+0.1*W(t)+0.05*Y(t-1)-0.01*A(t-1)))
+  ## initializations: zero
+  paramW <- c(0, 0.5, -0.25, 0.1)
+  qw <- sim$generateMechanism(paramW, family="gaussian")
+  paramA <- c(-0.1, 0.1, 0.25)
+  ga <- sim$generateMechanism(paramA, family="bernoulli")
+  paramY <- c(0.1, 0.1, 0.1, 0.05, -0.01)
+  Qy <- sim$generateMechanism(paramY, family="bernoulli")
+  data.dep <- sim$simulateBinaryWAY(nobs, qw=qw, ga=ga, Qy=Qy, verbose=log) ## very slow!
+}
