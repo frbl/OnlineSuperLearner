72ef7a1c4907f7fc24b33aae0a07a91343e35d88
Frank Blaauw <frank.blaauw@gmail.com>
2017-03-10 10:14:23 -0800

    Change the columnnames in the test

--- tests/testthat/test-Simulator.GAD.R
+++ tests/testthat/test-Simulator.GAD.R
@@ -5,7 +5,7 @@ sim <- Simulator.GAD$new()
 result <- sim$simulateWAY(by = nobs)
 
 test_that("it should return a dataframe with the correct columnames", {
-  expect_equal(colnames(result), c('W1', 'A1', 'Y1'))
+  expect_equal(colnames(result), c('W', 'A', 'Y'))
 })
 
 test_that("it should contain some non zero data", {
