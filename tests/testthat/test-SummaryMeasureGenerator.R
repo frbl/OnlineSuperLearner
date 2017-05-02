558734554fdaeb21fc0a75308af08dd3c0501d3e
Frank Blaauw <frank.blaauw@gmail.com>
2017-05-02 14:41:35 -0700

    Add normalization (#27)
    * Added scaling according to parameter bounds
    
    * Added specs for the scaling while getting the data

--- tests/testthat/test-SummaryMeasureGenerator.R
+++ tests/testthat/test-SummaryMeasureGenerator.R
@@ -19,6 +19,26 @@ test_that("it should set the minimal measurements needed correctly", {
   expect_equal(subject$minimal.measurements.needed, (max - 1))
 })
 
+test_that("it should throw when bounds are set but are not a list", {
+  mylist <- c(SMG.Mock$new())
+  expected_message =  "Argument 'bounds' is neither of nor inherits class list: character"
+  expect_error(described.class$new(SMG.list = mylist, bounds='notalist'), expected_message)
+})
+
+test_that("it should set normalized to false if no bounds are provided", {
+  mylist <- c(SMG.Mock$new())
+  subject <- described.class$new(SMG.list = mylist)
+  expect_false(subject$is_normalized)
+})
+
+test_that("it should set normalized to true and store the bounds whenever bounds are provided", {
+  bounds <-list(A=list(min=0, max=1000))
+  mylist <- c(SMG.Mock$new())
+  subject <- described.class$new(SMG.list = mylist, bounds = bounds)
+  expect_true(subject$is_normalized)
+  expect_equal(subject$get_bounds, bounds)
+})
+
 dataset <- data.table(Y= (seq(1,10)%%2) , A=rep(1,10), W=seq(10,1))
 
 context(" reset")
@@ -147,6 +167,30 @@ test_that("it should combine the results of multiple summarizers" , {
   expect_equal(result, expected)
 })
 
+test_that("it should scale the data if scaling is provided", {
+  data <- Data.Static$new(dataset = dataset)
+  bounds <-list(A=list(min=0, max=1000))
+
+  needed <- 1
+  mylist <- c(SMG.Mock$new(needed))
+  subject <- described.class$new(SMG.list = mylist, data = data, bounds = bounds)
+  for (i in 1:6) {
+    result <- subject$getNext()
+    cache  <- subject$getCache
+    expected  <- dataset[i,]
+
+    expect_true(is.a(result, 'data.frame'))
+
+    expect_equal(nrow(result), 1)
+    expect_equal(ncol(result), ncol(dataset))
+    expect_equal(result[,'Y'], expected[,'Y'])
+    expect_equal(result[,'W'], expected[,'W'])
+
+    # No explicit check for now, but at least it should not be the same (this one is scaled)
+    expect_false(result[,'A'] ==  expected[,'A'])
+  }
+})
+
 context(" checkEnoughDataAvailable")
 test_that("it should check if enough data is available for all formulae, and return true if all is available", {
   f1 <- RandomVariable$new(formula = y ~ w + w2 + w3 + a, family='gaussian')
@@ -183,3 +227,119 @@ context(" getNextN")
 test_that("it should be removed, this function is deprecated", {
  skip("Deprecated function, remove") 
 })
+
+context(" normalize")
+test_that("it should work with multi dimensional data", {
+  data <- data.table(
+                     Y =c(1,2,3,4,1,23,4,2,13,4),
+                     A =c(2,2,3,4,1,23,4,2,13,4)*10,
+                     W =c(3,2,3,4,1,23,4,2,13,4)*100
+                     )
+
+  bounds <- list()
+  for(name in colnames(data)) {
+    min_bound = min(data[, name, with=FALSE] )
+    max_bound = max(data[, name, with=FALSE] )
+    bounds <- append(bounds, list(list(max_bound = max_bound, min_bound = min_bound)))
+  }
+  names(bounds) <- colnames(data)
+
+  mylist <- c(SMG.Mock$new())
+  subject <- described.class$new(SMG.list = mylist)
+
+  res <- subject$normalize(data, bounds)
+
+  for(name in colnames(data)) {
+    expect_equal(min(res[, name, with=FALSE]), 0)
+    expect_equal(max(res[, name, with=FALSE]), 1)
+  }
+})
+
+test_that("it should work with multi dimensional data when bounds are provided for a subset of the variables", {
+  data <- data.table(
+    Y =c(1,2,3,4,1,23,4,2,13,4),
+    A =c(2,2,3,4,1,23,4,2,13,4)*10,
+    W =c(3,2,3,4,1,23,4,2,13,4)*100
+  )
+
+  bounds <- list()
+  for(name in colnames(data)) {
+    min_bound = min(data[, name, with=FALSE] )
+    max_bound = max(data[, name, with=FALSE] )
+    bounds <- append(bounds, list(list(max_bound = max_bound, min_bound = min_bound)))
+  }
+  names(bounds) <- colnames(data)
+  bounds <- bounds[-1]
+  not_changed <- setdiff(colnames(data), names(bounds))
+
+  expected_not_changed <- data[,not_changed, with=FALSE]
+
+  mylist <- c(SMG.Mock$new())
+  subject <- described.class$new(SMG.list = mylist)
+
+  res <- subject$normalize(data, bounds)
+
+  for(name in names(bounds)) {
+    expect_equal(min(res[, name, with=FALSE]), 0)
+    expect_equal(max(res[, name, with=FALSE]), 1)
+  }
+  expect_equal(res[,not_changed, with=FALSE], expected_not_changed)
+})
+
+test_that("it should throw if the provided data is not a data.table", {
+  wrong_datas <- list(NULL, list(), 'a')
+
+  mylist <- c(SMG.Mock$new())
+  subject <- described.class$new(SMG.list = mylist)
+  error_message <- "Argument 'data' is neither of nor inherits class data.table:"
+  lapply(wrong_datas, function(data) { 
+    current_error <- paste(error_message, class(data))
+    expect_error(subject$normalize(data), current_error) 
+  }) 
+})
+
+test_that("it should throw if the provided bounds are not a list", {
+  wrong_bounds <- list(NULL, data.frame, 'a')
+  data <- data.table(
+    Y =c(1,2,3,4,1,23,4,2,13,4),
+    A =c(2,2,3,4,1,23,4,2,13,4)*10,
+    W =c(3,2,3,4,1,23,4,2,13,4)*100
+  )
+
+  mylist <- c(SMG.Mock$new())
+  subject <- described.class$new(SMG.list = mylist)
+  error_message <- "Argument 'bounds' is neither of nor inherits class list:"
+  lapply(wrong_bounds, function(bounds) { 
+    current_error <- paste(error_message, class(bounds))
+    expect_error(subject$normalize(data, bounds), current_error) 
+  }) 
+})
+
+
+test_that("it should use the initialized bounds when no bounds are provided", {
+  data <- data.table(
+    Y =c(1,2,3,4,1,23,4,2,13,4),
+    A =c(2,2,3,4,1,23,4,2,13,4)*10,
+    W =c(3,2,3,4,1,23,4,2,13,4)*100
+  )
+
+  bounds <- list()
+  for(name in colnames(data)) {
+    min_bound = min(data[, name, with=FALSE] )
+    max_bound = max(data[, name, with=FALSE] )
+    bounds <- append(bounds, list(list(max_bound = max_bound, min_bound = min_bound)))
+  }
+  names(bounds) <- colnames(data)
+
+  mylist <- c(SMG.Mock$new())
+  subject <- described.class$new(SMG.list = mylist, bounds = bounds)
+
+  res <- subject$normalize(data)
+
+  for(name in colnames(data)) {
+    expect_equal(min(res[, name, with=FALSE]), 0)
+    expect_equal(max(res[, name, with=FALSE]), 1)
+  }
+ 
+})
+
