6fcc890b09273d08466fcfe946c62cb7bcff5a68
Frank Blaauw <frank.blaauw@gmail.com>
2017-05-08 18:51:37 -0700

    Cleaned up the local lm function (not yet working)

--- tests/testthat/test-ML.Local.lm.R
+++ tests/testthat/test-ML.Local.lm.R
@@ -1,4 +1,6 @@
 context("ML.Local.lm.R")
+described.class <- ML.Local.lm
+
 context(" initialize")
 test_that("it should accept the learning.rate and familyparameter", {
  expect_error(ML.Local.lm$new(learning.rate = 1, family = 'gaussian'), NA)
@@ -14,18 +16,17 @@ test_that("it should throw if a negative learning.rate is provided", {
  expect_error(ML.Local.lm$new(learning.rate = -1))
 })
 
-context(" predict")
+context(" do.predict")
 test_that("it should predict using a regression prediction if the family is gaussian", {
-  subject <- ML.Local.lm$new(family='gaussian')
+  subject <- described.class$new(family='gaussian')
   data <- data.table(
-                     x=c(1,2,3,4,5,6,7,8,9),
-                     y=c(9,8,7,6,5,4,3,2,1)
-                     )
+    x=c(1,2,3,4,5,6,7,8,9),
+    y=c(9,8,7,6,5,4,3,2,1)
+  )
   estimator <- glm(y ~ x, data, family = gaussian())
   data.new <-  data.table(x= c(1,11,111))
   expected <- predict(estimator, data.new)
-  subject$set_model(estimator$coefficients)
-  result <- subject$predict(data = data.new, A = NULL, W='x')
+  result <- subject$perform_prediction(X_mat = data.new, m.fit = list(coef = estimator))
   expect_true(all((result -  expected) == 0))
 })
 test_that("it should predict using a regression prediction if the family is binomial", {
@@ -37,9 +38,8 @@ test_that("it should predict using a regression prediction if the family is bino
   data.new <-  data.table(x= c(-1,1,10,11,12))
 
   estimator <- glm(y ~ x, data, family = binomial())
-  subject$set_model(estimator$coefficients)
   expected <- predict(estimator, data.new, type = 'response') > 0.5
-  result <- subject$predict(data = data.new, A = NULL, W='x') > 0.5
+  result <- subject$perform_prediction(X_mat = data.new, m.fit = list(coef = estimator)) > 0.5
 
   expect_true(all(expected == c(FALSE, FALSE, TRUE, TRUE, TRUE)))
   expect_true(all(result == expected))
@@ -111,4 +111,13 @@ context(" fit")
   #expect_true(abs(coeff.org[1] - intercept) >= abs(coeff.new[1] - intercept))
   #expect_true(abs(coeff.org[2] - intercept) >= abs(coeff.new[2] - intercept))
 #})
+
+context(" create_formula")
+test_that("it should create the correct formula based on the provided Y A and W", {
+  subject <- described.class$new()
+  Y <- 'outcome'
+  W <- c('input1', 'input2')
+  result <- subject$create_formula(Y,W)
+  expect_equal(result, 'outcome ~ input1 + input2' )
+})
 
