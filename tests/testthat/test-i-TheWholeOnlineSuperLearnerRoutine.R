7fcc6988e7b52b6b802dbd4db2e8dff44e5e03e6
Frank Blaauw <frank.blaauw@gmail.com>
2017-05-16 23:19:30 -0700

    Less iterations

--- tests/testthat/test-i-TheWholeOnlineSuperLearnerRoutine.R
+++ tests/testthat/test-i-TheWholeOnlineSuperLearnerRoutine.R
@@ -1,7 +1,7 @@
 context('Integration test: Test the whole SuperLearner routine')
 
 test_that("it should estimate the true treatment", {
-  if(Sys.getenv('CI') == "") skip('Only running this test on Circle. It takes very long.')
+  #if(Sys.getenv('CI') == "") skip('Only running this test on Circle. It takes very long.')
   # This very basic example shows how well the intervention estimation works. The procedure is as follows. We
   # have 3 variables, W A and Y, of which W is cts, A is binary and Y is gaussian. We will generate a number of
   # samples from this distribution with which the estimators are trained. Then we will simulate an interverntion
@@ -22,7 +22,7 @@ test_that("it should estimate the true treatment", {
   simulator  <- Simulator.GAD$new()
 
   # Number of items we have in our testset
-  training_set_size <- 2000
+  training_set_size <- 200
 
   # Number of iterations we want to use
   max_iterations = 3
@@ -35,7 +35,7 @@ test_that("it should estimate the true treatment", {
   algos <- list()
 
   # Number of iterations for approximation of the true parameter of interest
-  B <- 1e3
+  B <- 1e2
 
   # The intervention we are interested in
   intervention  <- list(when = c(2), what = c(0), variable ='A')
