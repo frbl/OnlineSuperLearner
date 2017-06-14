a252dd93abe0b84695c323b0b62ac790dfa46789
Frank Blaauw <frank.blaauw@gmail.com>
2017-06-14 15:39:33 +0200

    Fixed specs

--- tests/testthat/test-PreProcessor.R
+++ tests/testthat/test-PreProcessor.R
@@ -33,6 +33,37 @@ test_that("it should normalize data according to the provided bounds", {
   }
 })
 
+test_that("it should work with multi dimensional data when bounds are provided for a subset of the variables", {
+  data <- data.table(
+    Y =c(1,2,3,4,1,23,4,2,13,4),
+    A =c(2,2,3,4,1,23,4,2,13,4)*10,
+    W =c(3,2,3,4,1,23,4,2,13,4)*100
+  )
+
+  bounds <- PreProcessor.generate_bounds(data)
+  bounds <- bounds[-1]
+  not_changed <- setdiff(colnames(data), names(bounds))
+
+  expected_not_changed <- data[,not_changed, with=FALSE]
+  subject <- described.class$new(bounds = bounds)
+  res <- subject$normalize(data)
+
+  for(name in names(bounds)) {
+    expect_equal(min(res[, name, with=FALSE]), 0)
+    expect_equal(max(res[, name, with=FALSE]), 1)
+  }
+  expect_equal(res[,not_changed, with=FALSE], expected_not_changed)
+})
+
+test_that("it should throw if the provided data is not a data.table", {
+  wrong_datas <- list(NULL, list(), 'a')
+  subject <- described.class$new(bounds = list('123'))
+  error_message <- "Argument 'data' is neither of nor inherits class data.table"
+  lapply(wrong_datas, function(data) { 
+    expect_error(subject$normalize(data), error_message) 
+  }) 
+})
+
 context(" denormalize")
 test_that("it should denormalize data according to the provided bounds", {
 data <- data.table(
@@ -66,6 +97,12 @@ test_that("it should generate bounds with the correct entries", {
   lapply(bounds, function(bound) {
     expect_equal(names(bound), c('max_bound','min_bound'))
   })
+})
+
+test_that("it should throw if the provided data is not a data.table", {
+  wrong_datas <- list(NULL, list(), 'a')
+  error_message <- "Argument 'data' is neither of nor inherits class data.table"
+  expect_error(PreProcessor.generate_bounds(wrong_datas), error_message)
 })
 
 test_that("it should generate the correct bounds according to the min and max of the data", {
