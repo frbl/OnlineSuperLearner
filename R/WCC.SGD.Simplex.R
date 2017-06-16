c8b40be332cda53327d468f85800c3d6127640f9
Frank Blaauw <frank.blaauw@gmail.com>
2017-06-16 09:30:56 +0200

    Initial version of the SGD-Simplex optimizer (#31)

--- R/WCC.SGD.Simplex.R
+++ R/WCC.SGD.Simplex.R
@@ -1,5 +1,8 @@
 #' WCC.SGD.Simplex
-#' This is the SGD computer used in the Online SL package by David Benkeser; https://github.com/benkeser/onlinesl/
+#' This is the SGD computer used in the Online SL package by David Benkeser;
+#' https://github.com/benkeser/onlinesl/. It performs a gradient descent update
+#' (or a number of gradient descent updates) using the estimates of the
+#' separate estimators.
 #'
 #' @docType class
 #' @importFrom R6 R6Class
@@ -7,9 +10,11 @@ WCC.SGD.Simplex <- R6Class("WCC.SGD.Simplex",
   inherit = WeightedCombinationComputer,
   public =
     list(
-      initialize = function(weights.initial, step_size = 0.01, auto_update_stepsize = FALSE) {
+      initialize = function(weights.initial, step_size = 0.01, auto_update_stepsize = FALSE, iterations = 1000) {
         if(is.null(weights.initial)){
           throw('Please provide initial weights (or NA vector with the correct size)')
+        } else if (!all(is.na(weights.initial))) {
+          warning('The weights provided will be overridden by a random vector! If you dont want this message, please provide a vector (of the correct size) with NAs')
         }
         weights.initial <- runif(length(weights.initial), 0, 1)
         weights.initial <- weights.initial / sum(weights.initial)
@@ -17,57 +22,110 @@ WCC.SGD.Simplex <- R6Class("WCC.SGD.Simplex",
 
         private$auto_update_stepsize <- Arguments$getLogical(auto_update_stepsize)
         private$step_count <- 0
-        if (!private$auto_update_stepsize) {
-          private$step_size <- Arguments$getNumerics(step_size, c(0.000001, Inf))
-        }
+
+        private$step_size <- Arguments$getNumerics(step_size, c(0.0000000001, Inf))
+        private$iterations <- Arguments$getInteger(iterations, c(1, Inf))
       }
     ),
   active =
     list(
-    ),
-  private =
-    list(
-      step_size = NULL,
-      step_count = NULL,
-      auto_update_stepsize = NULL,
-      compute = function(Z, Y, libraryNames) {
-
-        grad <- - t(Z) %*% (Y - Z %*% private$weights)
-
-        new_weights <- private$weights - private$get_step_size * grad
-        names(new_weights) <- libraryNames
-
-        # Normalize the weights using the L1 simplex
-        new_weights_normalized <- private$project_to_l1_simplex(new_weights)
-        private$weights <- new_weights
-      },
-
       get_step_size = function() {
         if (private$auto_update_stepsize) {
           private$step_size <- 1 / private$increase_and_return_counter()
         } 
 
         private$step_size
+      }
+    ),
+  private =
+    list(
+      iterations = NULL,
+      step_size = NULL,
+      step_count = NULL,
+      auto_update_stepsize = NULL,
+
+      # This function performs several iterations (\code{private$iterations}) steps of gradient descent
+      # on the weight vector in \code{private$weights}.
+      # @param Y the true outcomes
+      # @param Z the estimated outcomes by each of the estimators
+      # @param libraryNames the names of the libraries for each of the weights
+      # @return a vector of new weights after \code{private$iterations} iterations
+      compute = function(Z, Y, libraryNames) {
+        current_weights <- self$get_weights
+        names(current_weights) <- libraryNames
+        #df <- data.table(t(current_weights))
+        for (i in seq(private$iterations)) {
+          current_weights <- private$get_updated_weights(Z=Z, Y=Y, libraryNames = libraryNames, current_weights = current_weights)
+          #df <- rbind(df, t(current_weights))
+        }
+        #plot.new()
+        #plot(df$a, ylim=range(c(0, 1)))
+        #par(new=TRUE)
+        #plot(df$b, ylim=range(c(0, 1)), col="red", axes = FALSE, xlab = "", ylab = "")
+        #par(new=TRUE)
+        #plot(df$c, ylim=range(c(0, 1)), col="orange", axes = FALSE, xlab = "", ylab = "")
+        #par(new=TRUE)
+        #plot(df$d, ylim=range(c(0, 1)), col="purple", axes = FALSE, xlab = "", ylab = "")
+        private$weights <- current_weights
+      },
+
+      # This function performs one step of gradient descent on the weight vector provided to it. 
+      # After computing these new weights, it projects them onto the L1 simplex, scaling them between
+      # 0 and 1 and letting them sum to 1
+      # @param Y the true outcomes
+      # @param Z the estimated outcomes by each of the estimators
+      # @param libraryNames the names of the libraries for each of the weights
+      # @param current_weights the weights we currently estimated for our estimators
+      # @param Y The outcome at iteration t
+      # @return a vector of new weights
+      get_updated_weights = function(Z, Y, libraryNames, current_weights) {
+        gradient <- private$compute_gradient(Z = Z, Y = Y, current_weights = current_weights)
+        new_weights <- current_weights - self$get_step_size * gradient
+
+        names(new_weights) <- libraryNames
+        # Normalize the weights using the L1 simplex
+        private$project_to_l1_simplex(new_weights)
       },
 
       increase_and_return_counter = function() {
         private$step_count <- private$step_count + 1
         private$step_count
       },
+
+      # This function computes the gradient of the current weights provided to it.
+      # @param Y the true outcomes
+      # @param Z the estimated outcomes by each of the estimators
+      # @param current_weights the weights we currently estimated for our estimators
+      # @return the gradient to be applied to the weights
+      compute_gradient = function(Z, Y, current_weights){
+        current_prediction <- Z %*% current_weights
+        grad <- - t(Z) %*% (Y - current_prediction) %>%
+          as.vector
+      },
       
+
+      # Function to project a vector of weights (weights) to the  L1-simplex. Essentially, this function
+      # makes sure that all values aree scaled between 0 and 1
+      # @param weights the vector of weights to be projected
+      # @return the vector projected onto L1 simplex
       project_to_l1_simplex = function(weights) {
         sorted_weights <- sort(weights, decreasing = TRUE)
+        sequence_weights <- weights %>% 
+          length %>% 
+          seq
+
         cumulative_sum_of_weights <- cumsum(sorted_weights) - 1
-        sequence_weights <- seq(length(weights))
         
         # By dividing the cumulative sum of weights (which was sorted), the lower weights will be
         # divided by a larger number 
         # (resulting in a larger numbers at the end if < 1 , lower numbers at the end if > 1)
         reweighted_cumulative_sum_of_weights <- cumulative_sum_of_weights / sequence_weights
 
-        K <- sorted_weights - reweighted_cumulative_sum_of_weights %>%
-          which(. > 0) %>%
-          rev(.)[1]
+        reweighted_cumulative_sum_of_weights
+
+        K <- sorted_weights - reweighted_cumulative_sum_of_weights
+        K <- which(K > 0)
+        K <- rev(K)[1]
 
         tau <- (sum(sorted_weights[seq(K)]) - 1) / K
 
--- tests/testthat/test-WCC.SGD.Simplex.R
+++ tests/testthat/test-WCC.SGD.Simplex.R
@@ -1,1 +1,61 @@
 context("WCC.SGD.Simplex.R")
+described.class <- WCC.SGD.Simplex
+set.seed(12345)
+n <- 1e5
+Z <- rnorm(n)
+
+pred <- cbind(Z, Z^2, Z^3, Z^4)
+true_params <- c(.2, .8, 0, 0)
+
+Y <- pred %*% true_params +rnorm(n)
+initial_weights <- rep(NA, 4) 
+libraryNames <- c('a', 'b', 'c', 'd')
+names(initial_weights) <- libraryNames
+
+context(' initialize')
+test_that("it should initialize with a random set of weights", {
+  subject <- described.class$new(weights.initial = initial_weights)
+  expect_false(any(is.na(subject$get_weights)))
+  expect_true(all(is(subject$get_weights, 'numeric')))
+})
+
+test_that("it should throw if initialized with a null weight vector", {
+  expect_error(
+    described.class$new(weights.initial = NULL),
+    "Please provide initial weights (or NA vector with the correct size)",
+    fixed = TRUE
+  )
+})
+
+test_that("it should give a warning if the provided vector is not NAs", {
+  expected_warning <- 'The weights provided will be overridden by a random vector! If you dont want this message, please provide a vector (of the correct size) with NAs'
+  expect_warning(described.class$new(weights.initial = c(1/4, 1/4, 1/4, 1/4)), expected_warning, fixed = TRUE)
+  expect_warning(described.class$new(weights.initial = initial_weights), NA)
+})
+
+context(' compute')
+test_that("it should compute the correct convex combination", {
+  set.seed(12345)
+  subject <- described.class$new(weights.initial = initial_weights, step_size = 0.0000001, iterations = 350)
+
+  subject$process(pred, Y, libraryNames)
+
+  # Check that we approximate the true parameters
+  difference <- subject$get_weights - true_params
+  expect_true(all(abs(difference) < 1e-2))
+})
+
+context(" get_step_size")
+test_that("it should should return the correct stepsize", {
+  step_size = 0.123
+  subject <- described.class$new(weights.initial = initial_weights, step_size = step_size)
+  expect_equal(subject$get_step_size, step_size)
+})
+
+test_that("it should update the stepsize if updating is enabled", {
+  step_size = 0.123
+  subject <- described.class$new(weights.initial = initial_weights, step_size = step_size, auto_update_stepsize = TRUE)
+  for (i in 1:10) {
+    expect_equal(subject$get_step_size, 1/i)
+  }
+})
