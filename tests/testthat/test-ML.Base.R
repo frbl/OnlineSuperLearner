b2f05500dc4c31952515204b536c301d9fcbf883
Frank Blaauw <frank.blaauw@gmail.com>
2017-07-26 11:59:13 +0200

    Added ml-base specs

--- tests/testthat/test-ML.Base.R
+++ tests/testthat/test-ML.Base.R
@@ -18,3 +18,32 @@ test_that("it should define a default lmclass", {
   expect_false(is.null(subject$lmclass))
   expect_equal(subject$lmclass, 'ML.BaseR6')
 })
+
+context("perform_prediction")
+test_that("it should call the private do.predict function", {
+  subject <- described.class$new()
+  expected <- 'performed prediction'
+  stub(subject$perform_prediction, 'private$do.predict', function(...) expected)
+  expect_equal(subject$perform_prediction(123), expected)
+})
+
+context("perform_fit")
+test_that("it should call the private do.fit function", {
+  subject <- described.class$new()
+  expected <- 'performed fit'
+  stub(subject$perform_fit, 'private$do.fit', function(...) expected)
+  expect_equal(subject$perform_fit(123), expected)
+})
+
+test_that("it should call the private do.fit function which should throw by default", {
+  subject <- described.class$new()
+  expect_error(subject$perform_fit(123), 'The fit method needs to be inherited')
+})
+
+context("perform_update")
+test_that("it should call the private do.update function", {
+  subject <- described.class$new()
+  expected <- 'performed update'
+  stub(subject$perform_update, 'private$do.update', function(...) expected)
+  expect_equal(subject$perform_update(123), expected)
+})
