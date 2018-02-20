150f87f0a98a2bfaff9693c8b98db3cfc5527718
Frank Blaauw <frank.blaauw@gmail.com>
2018-02-20 09:56:03 +0100

    Added SGD installation from github

--- inst/bash/install-package-dependencies.sh
+++ inst/bash/install-package-dependencies.sh
@@ -21,7 +21,6 @@ packages <- c(
 "e1071",
 "doRNG",
 "nnls",
-"sgd",
 "dplyr",
 "magrittr",
 "assertthat",
@@ -47,7 +46,8 @@ gh_packages <- list(
   list(repo = "osofr/condensier", branch = 'fb-add-update'),
   list(repo = "jimhester/covr", branch = 'master'),
   list(repo = "n-s-f/mockery", branch = 'master'),
-  list(repo = "r-lib/pkgdown", branch = 'master')
+  list(repo = "r-lib/pkgdown", branch = 'master'),
+  list(repo = "airoldilab/sgd", branch = 'master')
   #list(repo = "dmlc/xgboost/R-package", branch = 'master') #,
   #list(repo = 'cran/rkafka', branch = 'master')
 )
